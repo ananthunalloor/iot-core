da59d29a6c00d2e3502e65b3db02eb72719c0e79
ananthunalloor <ananthunalloor@gmail.com>
2024-10-20 12:40:09 +0000

    added basic sub demo

--- ros2_ws/sub_demo/sub_demo/subscriber_demo.py
+++ ros2_ws/sub_demo/sub_demo/subscriber_demo.py
@@ -1,17 +1,38 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String  # Change this to the correct message type used in ros2_mock_telemetry_topic
+from std_msgs.msg import String
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import json
+from sub_demo.connection_helper import ConnectionHelper
 
 class TelemetrySubscriber(Node):
     def __init__(self):
         super().__init__('telemetry_subscriber')
+        self.declare_parameter("path_for_config", "")
+        self.declare_parameter("discover_endpoints", False)
+
+        path_for_config = self.get_parameter("path_for_config").get_parameter_value().string_value
+        discover_endpoints = self.get_parameter("discover_endpoints").get_parameter_value().bool_value
+        self.connection_helper = ConnectionHelper(self.get_logger(), path_for_config, discover_endpoints)
+
         self.subscription = self.create_subscription(
-            String,  # Change this to the correct message type used in your topic
-            'ros2_mock_publish_topic',  # The name of the topic to subscribe to
+            String, 
+            'ros2_mock_publish_topic',
             self.listener_callback,
             10  # QoS (Quality of Service) profile
         )
-        self.subscription  # Prevent unused variable warning
+        self.subscription
+        self.init_subs()
+        
+    def init_subs(self):
+        """Subscribe to mock ros2_mock_publish_topic topic"""
+        self.subscription = self.create_subscription(
+            String,
+            'ros2_mock_publish_topic',
+            self.listener_callback,
+            10
+        ) 
 
     def listener_callback(self, msg):
         self.get_logger().info(f'Received message from ros2_mock_publish_topic: "{msg.data}"')
